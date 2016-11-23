67fec1abcd3a4d10aa05e8dd38e32ca20adf7a28
wonseop <wlee44@student.cccd.edu>
2016-11-22 16:36:09 -0800

    Added Child class

---
+++ app/src/test/java/edu/orangecoastcollege/cs273/gabyers/occparentportal/ChildTest.java
@@ -0,0 +1,75 @@
+package edu.orangecoastcollege.cs273.gabyers.occparentportal;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by wlee44 on 11/22/2016.
+ */
+public class ChildTest {
+
+    private Child mChild;
+
+    @Before
+    public void setUp() throws Exception {
+        mChild = new Child();
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getId() throws Exception {
+
+    }
+
+    @Test
+    public void setId() throws Exception {
+
+    }
+
+    @Test
+    public void getNameFirst() throws Exception {
+
+    }
+
+    @Test
+    public void setNameFirst() throws Exception {
+
+    }
+
+    @Test
+    public void getNameLast() throws Exception {
+
+    }
+
+    @Test
+    public void setNameLast() throws Exception {
+
+    }
+
+    @Test
+    public void getImagePath() throws  Exception {
+
+    }
+
+    @Test
+    public void setImagePath() throws Exception {
+
+    }
+
+    @Test
+    public void describeContents() throws Exception {
+
+    }
+
+    @Test
+    public void writeToParcel() throws Exception {
+
+    }
+
+}
